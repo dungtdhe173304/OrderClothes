2c7d59ceef073d39cfa336ea798f0f967708b13f
HuyNguyen3108 <huynqh3108@gmail.com>
2025-07-20 22:44:50 +0700

    Fix cart

---
+++ app/src/main/java/com/example/orderclothes/database/dao/CartItem.java
@@ -0,0 +1,4 @@
+package com.example.orderclothes.database.dao;
+
+public class CartItem {
+}
---
+++ app/src/main/java/com/example/orderclothes/models/ProductSize.java
@@ -0,0 +1,41 @@
+package com.example.orderclothes.models;
+
+public class ProductSize {
+    private int sizeId;
+    private int productId;
+    private String sizeName;
+    private int stockQuantity;
+
+    // Getters and Setters
+    public int getSizeId() {
+        return sizeId;
+    }
+
+    public void setSizeId(int sizeId) {
+        this.sizeId = sizeId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getSizeName() {
+        return sizeName;
+    }
+
+    public void setSizeName(String sizeName) {
+        this.sizeName = sizeName;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
